363f4475dd5bc0bbae12132e9c0c442a37e27862
Larry <larryoatmeal@gmail.com>
2020-03-19 15:34:15 -0400

    remove dist

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,4 +1,4 @@
-// production config
+``// production config
 const merge = require('webpack-merge');
 const {resolve} = require('path');
 
@@ -10,7 +10,7 @@ module.exports = merge(commonConfig, {
   output: {
     filename: 'js/bundle.[hash].min.js',
     path: resolve(__dirname, '../../dist'),
-    publicPath: '/',
+    publicPath: '/vgo_game', //so that it works with github pages
   },
   devtool: 'source-map',
   plugins: [],
